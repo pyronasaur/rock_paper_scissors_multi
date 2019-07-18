facc2f02344dcdc92a7e0ebfd38b1ffe2543f3e1
pyronasaur <pyronnicholas@gmail.com>
2019-07-18 19:36:55 -0400

    pushing firebaseConf

---
+++ assets/javascript/firebaseConf.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyArPecS8LJJ0N-7icQP5JlDx2MKvarpnGc",
+    authDomain: "rps-db-multi.firebaseapp.com",
+    databaseURL: "https://rps-db-multi.firebaseio.com",
+    projectId: "rps-db-multi",
+    storageBucket: "",
+    messagingSenderId: "691263166976",
+    appId: "1:691263166976:web:d31e2cd59b05375b"
+  };
